e3dfb9d4e133e44a518fbefe990e49aa8448506f
wahid <oheduzzamanshuvo@gmail.com>
2023-12-02 19:31:12 -0400

    added route and components in app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from "./containers/Home";
+import About from "./containers/About";
+import Contact from "./containers/Contact";
+import Listings from "./containers/Listings";
+import ListingDetail from "./containers/ListingDetail";
+import Login from "./containers/Login";
+import SignUp from "./containers/SignUp";
+import NotFound from "./components/NotFound"
+import Layout from "./hocs/Layout";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Provider} from "react-redux";
+import store from "./store";
+
+import './sass/main.scss'
+
+const App = () => {
+
+    return (
+        <Provider store={store}>
+            <Router>
+                <Layout>
+                    <Routes>
+                        <Route path='/' element={<Home/>}/>
+                        <Route path='/about' element={<About/>}/>
+                        <Route path='/contact' element={<Contact/>}/>
+                        <Route path='/listings' element={<Listings/>}/>
+                        <Route path='/listings/:id' element={<ListingDetail/>}/>
+                        <Route path='/login' element={<Login/>}/>
+                        <Route path='/signup' element={<SignUp/>}/>
+                        <Route path='*' element={<NotFound/>}/>
+                    </Routes>
+                </Layout>
+            </Router>
+        </Provider>
+    );
 }
 
 export default App;
